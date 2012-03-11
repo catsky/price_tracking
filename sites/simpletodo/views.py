d82d0871c54196795cf5462c15bbb42f4e5008cb
catsky <zhdhui@gmail.com>
2012-03-11 21:36:50 +0800

    update link

--- sites/simpletodo/views.py
+++ sites/simpletodo/views.py
@@ -50,7 +50,7 @@ def new(request):
     if request.method == "POST":
         form = AddForm(request.POST)
         if form.is_valid():
-            src = urllib2.urlopen('http://www.amazon.com/George-F-Kennan-American-Life/dp/1594203121/ref=amb_link_359769522_5?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=center-2&pf_rd_r=0NXCXBM9BA5AXRWGQ51T&pf_rd_t=101&pf_rd_p=1350587622&pf_rd_i=283155')
+            src = urllib2.urlopen(request.POST['productLink'])
             parser = BeautifulSoup(src)
             title_p = parser.find('span', {'id':'btAsinTitle'})
             title = title_p.contents[0]
